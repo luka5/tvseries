f059b0f4aa1fe7f66daea27fc2c73a7f7279c46d
luka5 <github@luka5.de>
2012-01-25 11:55:27 +0100

    Episode_Model setPremier und setOriginalPremier: wird immer an d-m-Y gespeichert

--- dynamic/Model/Abstract.php
+++ dynamic/Model/Abstract.php
@@ -2,6 +2,12 @@
 
 	abstract class Model_Abstract{
 
+		protected function convertDate($datastring){
+			
+			$date = new DateTime($datastring);
+			return $date->format("d-m-Y");
+			
+		}
+		
 	}
-
 ?>
