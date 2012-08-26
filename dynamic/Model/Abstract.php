a5d6093cacd1f81d31c90eaeea200ca3c26edf6b
luka5 <github@luka5.de>
2012-08-26 15:46:47 +0200

    bugfix no date

--- dynamic/Model/Abstract.php
+++ dynamic/Model/Abstract.php
@@ -7,7 +7,7 @@
 			$germanMonths = array("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");
 			$englishMonths = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 			$datastring = str_replace($germanMonths, $englishMonths, $datastring);
-			if($datastring == ""){
+			if($datastring == "" || $datastring == "0000-00-00"){
 				return "";
 			}else{
 				$date = new DateTime($datastring);
